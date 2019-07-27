620f4f007524beefce5374c8d5110f48d672a17c
mnenonen@student.hive.fi <mnenonen@c3r4p5.hive.fi>
2019-07-27 16:42:50 +0300

    We're printing the name of function and its whole string

--- ex00/ft_rush_collector.c
+++ ex00/ft_rush_collector.c
@@ -6,12 +6,11 @@
 /*   By: sreijola <sreijola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 14:05:03 by sreijola          #+#    #+#             */
-/*   Updated: 2019/07/27 16:32:37 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/27 16:41:23 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_rush.h"
-
 
 t_rush_info	*ft_rush_collector00(int x, int y)
 {
@@ -23,19 +22,19 @@ t_rush_info	*ft_rush_collector00(int x, int y)
 	ans = (char *)malloc((x * y + 1) * sizeof(char));
 	i = 1;
 	if (y == 1)
-		ft_strncpy(ans, ft_top00(x), x);
+		strncpy(ans, ft_top00(x), x);
 	else if (y > 1)
 	{
-		ft_strncpy(ans, ft_top00(x), x);
+		strncpy(ans, ft_top00(x), x);
 		while (i <= y - 2)
 		{
-			ft_strncpy(&ans[i * x], ft_mid00(x), x);
+			strncpy(&ans[i * x], ft_mid00(x), x);
 			i++;
 		}
-		ft_strncpy(ans[(y - 1) * x], ft_bot00(x), x);
+		strncpy(&ans[(y - 1) * x], ft_bot00(x), x);
 		ans[y * x] = '\0';
 	}
-	ft_strcpy(myinfo->name, "rushcollector00");
+	strcpy(myinfo->name, "rushcollector00");
 	myinfo->print = ans;
 	return (myinfo);
 }
