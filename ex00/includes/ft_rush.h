2bb33e693e04989e92c29b4d1d1f900115ca0edc
Satu Reijola <sreijola@c3r5p4.hive.fi>
2019-07-27 17:49:45 +0300

    listed top, bot and mid fs too

--- ex00/includes/ft_rush.h
+++ ex00/includes/ft_rush.h
@@ -6,7 +6,7 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 14:01:22 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/07/27 17:12:55 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/27 17:48:55 by sreijola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,17 @@ char	*ft_strncpy(char *str1, const char *str2, int len);
 char	*ft_top00(int len);
 char	*ft_bot00(int len);
 char	*ft_mid00(int len);
+char	*ft_top01(int len);
+char	*ft_bot01(int len);
+char	*ft_mid01(int len);
+char	*ft_top02(int len);
+char	*ft_bot02(int len);
+char	*ft_mid02(int len);
+char	*ft_top03(int len);
+char	*ft_bot03(int len);
+char	*ft_mid03(int len);
+char	*ft_top04(int len);
+char	*ft_bot04(int len);
+char	*ft_mid04(int len);
 
 #endif
