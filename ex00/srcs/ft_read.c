61ebb480a91b7585db5ddfe2b8703fad7d0d4a48
mnenonen@student.hive.fi <mnenonen@c3r4p5.hive.fi>
2019-07-27 22:00:36 +0300

    ft_read: cured

--- ex00/srcs/ft_read.c
+++ ex00/srcs/ft_read.c
@@ -6,7 +6,7 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 11:08:43 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/07/27 21:56:23 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/27 22:00:00 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,67 +15,9 @@
 
 int	ft_read(void)
 {
-	int			fd;
-	int			reading;
-	void		*buf;
-	char		first_line[10 * BUFFER_SIZE];
-	int			letters;
-	t_rush_info	*rush00;
-
-	buf = (void *)malloc((1 + BUFFER_SIZE) * sizeof(char));
-//	fd = open(0, O_RDONLY);
-//	if (fd == -1)
-//		return (1);
-	reading = 1;
-	letters = 0;
-	while (reading)
-	{
-		if (reading != '\n')
-		{
-			reading = read(0, buf, 1);
-			write(1, buf, 1);
-			first_line[letters] = ((char *)buf)[letters];
-			++letters;
-		}
-		rush00 = ft_rush_collector00(letters, 1);
-		if (!strcmp(strncpy(NULL, first_line, letters), strncpy(NULL,
-					rush00->print, letters)))
-			ft_putstr("it could be top00");
-		else
-			ft_putstr("No match found");
-	}
-
-// file open
-// read a char
-// while char != '\0'
-// 		FIRST LINE
-// 		while char != '\n'
-// 			construct string
-// 			measure x
-// 		compare string to known fn(x)s
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
-//		SECOND LINE
-//		while char != '\n'
-//			construct string
-//			measure x_2
-//		if (x_2 != x_1)
-/// 		compare string to known fn(x)s with y(1) || y(2) || y(3)
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
-//		THIRD LINE
-//		while char != '\n'
-//			construct string
-//			measure x_3
-//		if (x_3 != x_1)
-/// 		compare string to known fn(x)s with y(1) || y(3) || y(4)
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
+	char *buf;
+	buf = (void *)malloc((10) * sizeof(char));
+	read(0, buf, 9);
+	write(1, buf, 9);
 	return (0);
 }
