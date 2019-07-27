2d497a07676cf5e2613c03f800b52dcf82a6c96d
Satu Reijola <sreijola@c3r5p4.hive.fi>
2019-07-27 15:55:04 +0300

    changes to collector and h

--- ex00/ft_rush_collector.c
+++ ex00/ft_rush_collector.c
@@ -6,21 +6,24 @@
 /*   By: sreijola <sreijola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 14:05:03 by sreijola          #+#    #+#             */
-/*   Updated: 2019/07/27 15:27:05 by sreijola         ###   ########.fr       */
+/*   Updated: 2019/07/27 15:53:53 by sreijola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_top.h"
 
-char	*ft_rush_collector00(int x, int y)
+
+t_rush_info	*ft_rush_collector00(int x, int y)
 {
 	char	*ans;
 	int		i;
+	t_rush_info *myinfo;
 
+	myinfo = (t_rush_info *)malloc(sizeof(t_rush_info));
 	ans = (char *)malloc((x * y + 1) * sizeof(char));
 	i = 1;
 	if (y == 1)
-		ans[0] = ft_top00(x);
+		ft_strncpy(ans, ft_top00(x), x);
 	else if (y > 1)
 	{
 		ans[0] = ft_top00(x);
@@ -32,16 +35,20 @@ char	*ft_rush_collector00(int x, int y)
 		ans[y - 1] = ft_bot00(x);
 		ans[y] = '\0';
 	}
-	return (ans);
+	ft_strcpy(myinfo->name, "rushcollector00");
+	myinfo->print = ans;
+	return (myinfo);
 }
 
-int			main()
+int			main(void)
 {
 	int length = 9;
 	char * first_line0;
+	t_rush_info	*myinfo;
 
-	first_line0 = ft_rush_collector00(length, 3);
-	printf("%s \n", first_line0);
-	free (first_line0);
+	myinfo = ft_rush_collector00(length, 3);
+	printf("%s \n", myinfo->print);
+	free (myinfo->print);
+	free (myinfo);
 	return (0);
 }
