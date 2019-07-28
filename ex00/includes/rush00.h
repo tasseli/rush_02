6d3609e80c3a710eb7380958e230ccc59aa5259c
Nataliya Zinovyeva <nzinovye@c3r5p6.hive.fi>
2019-07-28 14:12:05 +0300

    changes for test rush00

--- ex00/includes/rush00.h
+++ ex00/includes/rush00.h
@@ -6,8 +6,33 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/28 13:11:28 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/07/28 13:11:33 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/28 14:08:17 by nzinovye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef RUSH00_H
+# define RUSH00_H
+
+# include <unistd.h> // read, close
+
+# include <sys/uio.h> // write?
+# include <sys/types.h> // open
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# include <string.h> // TODO remove
+
+# define BUFFER_SIZE 1001
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+//char	*ft_strncpy(char *str, const char *str, int n); Under repairs.
+int	ft_read(void);
+
+t_rush_info	*ft_rush_collect_test01(int x, int y);
+
+
+
+
+#endif
 
--- ex00/srcs/rush00_main.c
+++ ex00/srcs/rush00_main.c
@@ -6,11 +6,11 @@
 /*   By: sreijola <sreijola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/28 13:14:34 by sreijola          #+#    #+#             */
-/*   Updated: 2019/07/28 13:17:55 by sreijola         ###   ########.fr       */
+/*   Updated: 2019/07/28 14:04:34 by nzinovye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_rush.h"
+#include "rush00.h"
 #include "ft_read.h"
 
 void	testprint(char *masterstr, int x)
@@ -34,38 +34,15 @@ int	main(int argc, char **argv)
 	int y = atoi(argv[2]);
 	int			length = x;
 	char		*first_line0;
-	/*t_rush_info	*myinfo0;*/
 	t_rush_info	*myinfo1;
-	/*t_rush_info	*myinfo2;
-	t_rush_info	*myinfo3;
-	t_rush_info	*myinfo4;
-
-	/*myinfo0 = ft_rush_collector00(length, y);
-	printf("%s %s\n", myinfo0->name, myinfo0->print);
-	testprint(myinfo0->print, x);
-	free (myinfo0->print);
-	free (myinfo0);*/
+	
 	myinfo1 = ft_rush_collect_test01(length, y);
 	printf("%s %s\n", myinfo1->name, myinfo1->print);
 	testprint(myinfo1->print, x);
 	free (myinfo1->print);
 	free (myinfo1);
-	/*myinfo2 = ft_rush_collector02(length, y);
-	printf("%s %s\n", myinfo2->name, myinfo2->print);
-	testprint(myinfo2->print, x);
-	free (myinfo2->print);
-	free (myinfo2);
-	myinfo3 = ft_rush_collector03(length, y);
-	printf("%s %s\n", myinfo3->name, myinfo3->print);
-	testprint(myinfo3->print, x);
-	free (myinfo3->print);
-	free (myinfo3);
-	myinfo4 = ft_rush_collector04(length, y);
-	printf("%s %s\n", myinfo4->name, myinfo4->print);
-	testprint(myinfo4->print, x);
-	free (myinfo4->print);
-	free (myinfo4);*/
-	ft_putchar('0' + ft_read());
+
+	ft_read();
 	return (0);
 }
 
