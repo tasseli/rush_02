6c881489065ce640d62e8e45c5dbe5845158369d
mnenonen@student.hive.fi <mnenonen@c3r4p5.hive.fi>
2019-07-28 21:53:13 +0300

    handling 0 cases, optimising ft_read.c

--- ex00/srcs/ft_read.c
+++ ex00/srcs/ft_read.c
@@ -6,7 +6,7 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 11:08:43 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/07/28 21:50:51 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/28 21:52:53 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,37 +115,3 @@ int		find_matches(void *buf, t_coords *coords_found)
 	}
 	return (found);
 }
-
-
-// file open
-// read a char
-// while char != '\0'
-// 		FIRST LINE
-// 		while char != '\n'
-// 			construct string
-// 			measure x
-// 		compare string to known fn(x)s
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
-//		SECOND LINE
-//		while char != '\n'
-//			construct string
-//			measure x_2
-//		if (x_2 != x_1)
-/// 		compare string to known fn(x)s with y(1) || y(2) || y(3)
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
-//		THIRD LINE
-//		while char != '\n'
-//			construct string
-//			measure x_3
-//		if (x_3 != x_1)
-/// 		compare string to known fn(x)s with y(1) || y(3) || y(4)
-// 		narrow down
-// 		if (no matches left)
-// 			end
-
